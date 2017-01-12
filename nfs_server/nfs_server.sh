b1475ff09ba96acf7a3668f7ef88ddca273676c3
Kevin Grigorenko <kevin@myplaceonline.com>
2017-01-11 20:04:11 -0800

    Support Debian/Ubuntu in nfs_server cube

--- nfs_server/nfs_server.sh
+++ nfs_server/nfs_server.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
-cube_package install nfs-utils
+if cube_operating_system_has_flavor ${POSIXCUBE_OS_FLAVOR_FEDORA}; then
+  cube_package install nfs-utils
+  cubevar_nfs_server_service="nfs-server"
+elif cube_operating_system_has_flavor ${POSIXCUBE_OS_FLAVOR_DEBIAN}; then
+  # https://help.ubuntu.com/lts/serverguide/network-file-system.html
+  cube_package install nfs-kernel-server
+  cubevar_nfs_server_service="nfs-kernel-server"
+else
+  cube_throw Not implemented
+fi
 
 cube_ensure_directory "${cubevar_app_nfs_server_directory}" 700
 
-cube_set_file_contents "/etc/sysconfig/rpcbind" "templates/rpcbind"
+if cube_operating_system_has_flavor ${POSIXCUBE_OS_FLAVOR_FEDORA}; then
+  cube_set_file_contents "/etc/sysconfig/rpcbind" "templates/rpcbind"
 
-cube_service enable rpcbind
-cube_service start rpcbind
-
-if cube_set_file_contents "/etc/exports" "templates/exports.template" ; then
-  cube_service restart nfs-server
+  cube_service enable rpcbind
+  cube_service start rpcbind
 fi
 
-cube_service enable nfs-server
-cube_service start nfs-server
+if cube_set_file_contents "/etc/exports" "templates/exports.template" ; then
+  cube_service restart ${cubevar_nfs_server_service}
+fi
+
+cube_service enable ${cubevar_nfs_server_service}
+cube_service start ${cubevar_nfs_server_service}
