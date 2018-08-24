d9738b09260289bdcf52fc5dc583b41275b01866
Kevin G <kevin@myplaceonline.com>
2018-08-23 21:48:43 -0700

    Use pretty formatting for psql

--- local/sql_rw.sh
+++ local/sql_rw.sh
@@ -8,6 +8,6 @@ export POSIXCUBE_COLORS=true
 
 LOCALDBHOST="$(echo "${cubevar_app_primary_host_db_public}" | sed 's/\./-internal./')"
 
-ssh root@${cubevar_app_primary_host_db_public} "PGPASSWORD=\"${cubevar_app_passwords_postgresql_myplaceonline}\" psql -U myplaceonline -h ${LOCALDBHOST} -d myplaceonline_production -c \"${@}\""
+ssh root@${cubevar_app_primary_host_db_public} "PGPASSWORD=\"${cubevar_app_passwords_postgresql_myplaceonline}\" psql -x -U myplaceonline -h ${LOCALDBHOST} -d myplaceonline_production -c \"${@}\""
 
 popd
