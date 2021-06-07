4f2cf9874c429c17ed98950a3576ecd5a6608e12
Kevin G <kevin@myplaceonline.com>
2021-06-06 17:19:00 -0700

    Update backup script

--- local/backup.sh
+++ local/backup.sh
@@ -19,27 +19,25 @@ POSIXCUBE_SOURCED=true . posixcube.sh source; POSIXCUBE_SOURCED=
 export POSIXCUBE_COLORS=true
 
 # Write to the root directory because /tmp is limited in some clouds
-OUTPUTFILE=/myplaceonline_backup_$(date +"%Y%m%d_%H%M")_pgdump.sql
-LARGEFILES=/myplaceonline_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
-PRIMARYLARGEFILES=/myplaceonline_primary_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
-LOCALDBHOST="$(echo "${cubevar_app_backup_host_db_public}" | sed 's/\./-internal./')"
+DBBACKUP=/myplaceonline_db_backup_$(date +"%Y%m%d_%H%M")_pgdump.sql
+LARGEFILES=/myplaceonline_largefiles_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
+EMAILFILES=/myplaceonline_email_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
 
-time ssh root@${cubevar_app_primary_host_db_public} "tar czvf ${PRIMARYLARGEFILES} /var/vmail/"
+time ssh root@${cubevar_app_email_host} "tar czvf ${EMAILFILES} /var/vmail/"
 
-scp root@${cubevar_app_primary_host_db_public}:${PRIMARYLARGEFILES} ${OUTPUTDIR} && \
-  ssh root@${cubevar_app_primary_host_db_public} "rm -f ${PRIMARYLARGEFILES};"
+scp root@${cubevar_app_email_host}:${EMAILFILES} ${OUTPUTDIR} && \
+  ssh root@${cubevar_app_email_host} "rm -f ${EMAILFILES};"
 
-# Stop elasticsearch temporarily because otherwise we might run out of memory
-time ssh root@${cubevar_app_backup_host_db_public} "systemctl stop elasticsearch; systemctl stop influxd; sudo -i -u postgres psql myplaceonline_production -c 'select pg_xlog_replay_pause();' && sudo -i -u postgres psql myplaceonline_production -c 'select  pg_is_xlog_replay_paused();' && PGPASSWORD=${cubevar_app_passwords_postgresql_myplaceonline} /usr/bin/pg_dump -U myplaceonline -h ${LOCALDBHOST} -d myplaceonline_production -Fc > ${OUTPUTFILE} && sudo -i -u postgres psql myplaceonline_production -c 'select pg_xlog_replay_resume();'; systemctl start elasticsearch; systemctl start influxd;"
+time ssh root@${cubevar_app_backup_node} "PGPASSWORD=${cubevar_app_passwords_postgresql_myplaceonline} PGSSLMODE=allow /usr/bin/pg_dump -U myplaceonline -h ${cubevar_app_db_host} -p 25060 -d myplaceonline_production -Fc > ${DBBACKUP}"
 
-time scp root@${cubevar_app_backup_host_db_public}:${OUTPUTFILE} ${OUTPUTDIR} && \
-  ssh root@${cubevar_app_backup_host_db_public} "rm -f ${OUTPUTFILE}" && \
-  ssh root@${cubevar_app_backup_host_db_public} "tar czvf ${LARGEFILES} /var/lib/remotenfs/" && \
-  scp root@${cubevar_app_backup_host_db_public}:${LARGEFILES} ${OUTPUTDIR} && \
-  ssh root@${cubevar_app_backup_host_db_public} "rm -f ${LARGEFILES};" && \
+time scp root@${cubevar_app_backup_node}:${DBBACKUP} ${OUTPUTDIR} && \
+  ssh root@${cubevar_app_backup_node} "rm -f ${DBBACKUP}" && \
+  ssh root@${cubevar_app_backup_node} "tar czvf ${LARGEFILES} /var/lib/remotenfs/" && \
+  scp root@${cubevar_app_backup_node}:${LARGEFILES} ${OUTPUTDIR} && \
+  ssh root@${cubevar_app_backup_node} "rm -f ${LARGEFILES};" && \
   echo "Done. To encrypt:"
-  echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${OUTPUTFILE#/}.pgp --symmetric ${OUTPUTFILE#/}; rm ${OUTPUTFILE#/};"
+  echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${DBBACKUP#/}.pgp --symmetric ${DBBACKUP#/}; rm ${DBBACKUP#/};"
   echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${LARGEFILES#/}.pgp --symmetric ${LARGEFILES#/}; rm ${LARGEFILES#/}"
-  echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${PRIMARYLARGEFILES#/}.pgp --symmetric ${PRIMARYLARGEFILES#/}; rm ${PRIMARYLARGEFILES#/}"
+  echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${EMAILFILES#/}.pgp --symmetric ${EMAILFILES#/}; rm ${EMAILFILES#/}"
 
 popd
