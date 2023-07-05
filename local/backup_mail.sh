cfa724ae73e700dd5bdf668151c919357fda9146
Kevin G <kevin@myplaceonline.com>
2023-07-04 23:47:06 -0500

    Update script

--- local/backup_mail.sh
+++ local/backup_mail.sh
@@ -11,7 +11,7 @@ export POSIXCUBE_COLORS=true
 # Write to the root directory because /tmp is limited in some clouds
 EMAILFILES=/myplaceonline_email_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
 
-time ssh root@${cubevar_app_email_host} "tar czvf ${EMAILFILES} /var/vmail/"
+time ssh root@${cubevar_app_email_host} "tar czvf ${EMAILFILES} /mnt/mailvolume/"
 
 scp root@${cubevar_app_email_host}:${EMAILFILES} ${OUTPUTDIR} && \
   ssh root@${cubevar_app_email_host} "rm -f ${EMAILFILES};"
