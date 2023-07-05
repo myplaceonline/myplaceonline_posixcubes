897cff8539e9edf84ab0284ba97fcfe698876284
Kevin G <kevin@myplaceonline.com>
2023-07-04 23:49:28 -0500

    Update

--- local/backup.sh
+++ local/backup.sh
@@ -21,12 +21,6 @@ export POSIXCUBE_COLORS=true
 # Write to the root directory because /tmp is limited in some clouds
 DBBACKUP=/myplaceonline_db_backup_$(date +"%Y%m%d_%H%M")_pgdump.sql
 LARGEFILES=/myplaceonline_largefiles_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
-EMAILFILES=/myplaceonline_email_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
-
-time ssh root@${cubevar_app_email_host} "tar czvf ${EMAILFILES} /var/vmail/"
-
-scp root@${cubevar_app_email_host}:${EMAILFILES} ${OUTPUTDIR} && \
-  ssh root@${cubevar_app_email_host} "rm -f ${EMAILFILES};"
 
 time ssh root@${cubevar_app_backup_node} "PGPASSWORD=${cubevar_app_passwords_postgresql_myplaceonline} PGSSLMODE=allow /usr/bin/pg_dump -U myplaceonline -h ${cubevar_app_db_host} -p 25060 -d myplaceonline_production -Fc > ${DBBACKUP}"
 
@@ -38,6 +32,5 @@ time scp root@${cubevar_app_backup_node}:${DBBACKUP} ${OUTPUTDIR} && \
   echo "Done. To encrypt:"
   echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${DBBACKUP#/}.pgp --symmetric ${DBBACKUP#/}; rm ${DBBACKUP#/};"
   echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${LARGEFILES#/}.pgp --symmetric ${LARGEFILES#/}; rm ${LARGEFILES#/}"
-  echo "gpg --s2k-mode 3 --s2k-count 65536 --force-mdc --cipher-algo AES256 --s2k-digest-algo sha512 -o ${EMAILFILES#/}.pgp --symmetric ${EMAILFILES#/}; rm ${EMAILFILES#/}"
 
 popd
