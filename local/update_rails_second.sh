38b5d29736664fae6b9f5f16f852b83d3fecaaa9
Kevin G <kevin@myplaceonline.com>
2023-12-24 09:56:48 -0600

    Add Script

---
+++ local/update_rails_second.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+pushd "$(dirname "$0")/../"
+
+POSIXCUBE_SOURCED=true . posixcube.sh source; POSIXCUBE_SOURCED=
+
+#export POSIXCUBE_COLORS=true
+
+posixcube.sh -R -u root -h web59.myplaceonline.com -O cubevar_app_servers=*.myplaceonline.com -O cubevar_app_web_servers=web* -U firewall_whitelist -U nfs_client -c web_begin -c core_begin -c postfix_sender -c web -c core_end
+
+popd
