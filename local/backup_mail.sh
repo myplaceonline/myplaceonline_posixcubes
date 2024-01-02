243395797219718bd833917d05d63a800a1ce68e
Kevin G <kevin@myplaceonline.com>
2024-01-01 21:11:48 -0600

    Simplify backup scripts

--- local/backup_mail.sh
+++ local/backup_mail.sh
@@ -8,12 +8,6 @@ POSIXCUBE_SOURCED=true . posixcube.sh source; POSIXCUBE_SOURCED=
 
 export POSIXCUBE_COLORS=true
 
-# Write to the root directory because /tmp is limited in some clouds
-EMAILFILES=/myplaceonline_email_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
-
-time ssh root@${cubevar_app_email_host} "tar czvf ${EMAILFILES} /mnt/mailvolume/"
-
-scp root@${cubevar_app_email_host}:${EMAILFILES} ${OUTPUTDIR} && \
-  ssh root@${cubevar_app_email_host} "rm -f ${EMAILFILES};"
+time ssh root@${cubevar_app_backup_node} "tar czvf - /mnt/mailvolume/" > ${OUTPUTDIR}/myplaceonline_email_backup_$(date +"%Y%m%d_%H%M")_files.tar.gz
 
 popd
